fc4753e7c5da630a63a208bd46c8c3f077b5bf25
Jash Kothari <jash@edcast.com>
2025-02-18 11:52:42 +0530

    * Rails branch.

---
+++ lib/paperclip/io_adapters/http_url_proxy_adapter.rb
@@ -0,0 +1,16 @@
+module Paperclip
+  class HttpUrlProxyAdapter < UriAdapter
+    def self.register
+      Paperclip.io_adapters.register self do |target|
+        String === target && target =~ REGEXP
+      end
+    end
+
+    REGEXP = /\Ahttps?:\/\//
+
+    def initialize(target, options = {})
+      escaped = URI::DEFAULT_PARSER.escape(target)
+      super(URI(target == URI::DEFAULT_PARSER.unescape(target) ? escaped : target), options)
+    end
+  end
+end
